0cb8d26bb9ff0aa532e0258c9c7d078583628d69
Phan-dev0 <vegarphan@gmail.com>
2023-09-27 18:14:18 +0700

    update fe

--- bus-map/src/components/map/Map.jsx
+++ bus-map/src/components/map/Map.jsx
@@ -20,51 +20,44 @@ import Stack from '@mui/material/Stack';
 import { Autocomplete, IconButton, Input, InputBase, Paper, TextField } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import { Form } from "react-router-dom";
+import { TextFields } from "@mui/icons-material";
 
 export default function Map() {
 
   const position = [10.8231, 106.6297];
-  const minLatitude = 10.7;
-  const minLongitude = 106.5;
-  const maxLatitude = 10.9;
-  const maxLongitude = 106.7;
+
   const customIcon = new Icon({
     iconUrl: require("../../assets/img/mark.png"),
     iconSize: [38, 38], // size of the icon
   });
 
-
   const [active, setActive] = useState(false);
-  const [address, setAddress] = useState(["", ""]);
+  const [address, setAddress] = useState("");
 
-  const [strAddress, setStrAddress] = useState([[], []]);
+  const [strAddress, setStrAddress] = useState({
+    latitude: "",
+    longitude: ""
+  });
 
-
-  const fetchAdd = async (ar, ar2) => {
+  const fetchAddressInfo = async (ar) => {
     // const ar = "62 Gò Vấp, Việt Nam";
     const apiUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
       ar
     )}&format=json`;
-    const apiUrl2 = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
-      ar2
-    )}&format=json`;
-    console.log(apiUrl);
-    try {
-      const response1 = await fetch(apiUrl);
-      const data1 = await response1.json();
 
-      const response2 = await fetch(apiUrl2);
-      const data2 = await response2.json();
-      if ((data1.length > 0) & (data2.length > 0)) {
-        const latitude1 = data1[0].lat;
-        const longitude1 = data1[0].lon;
-        const latitude2 = data2[0].lat;
-        const longitude2 = data2[0].lon;
-        setStrAddress([
-          [{ latitude1 }, { longitude1 }],
-          [{ latitude2 }, { longitude2 }],
-        ]);
-      }
+    console.log(apiUrl);
+
+    try {
+      const response = await fetch(apiUrl);
+      const addressInfo = await response.json();
+   
+      const latitude1 = addressInfo[0].lat;
+      const longitude1 = addressInfo[0].lon;
+      setStrAddress({
+        latitude: latitude1,
+        longitude: longitude1
+      });
+
       // Tiếp tục xử lý dữ liệu ở đây
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -77,43 +70,27 @@ export default function Map() {
     setAddress(newAddress);
   };
 
-  const handleInputChange2 = (event) => {
-    const newAddress = [...address];
-    newAddress[1] = event.target.value;
-    setAddress(newAddress);
-  };
+  // const demo = () => {
 
-  const demo = () => {
-
-    setStrAddress([[], []]);
-    fetchAdd(address[0], address[1]);
-    setActive(!active);
-  };
+  //   setStrAddress([[], []]);
+  //   fetchAdd(address[0]);
+  //   setActive(!active);
+  // };
 
 
   // feath api
   const fetchVietNameInfo = async () =>{
     try {
       const response = await fetch("https://provinces.open-api.vn/api/?depth=3");
-      if (!response.ok) {
+      const responseDataSaiGon = await fetch("https://cdn.jsdelivr.net/gh/thien0291/vietnam_dataset@1.0.0/data/SG.json");
+      
+      if (!responseDataSaiGon.ok) {
         throw new Error('Network response was not ok');
       }
-      
-      const data = await response.json();
+      const data = await responseDataSaiGon.json();
 
-      for(let i = 0; i < data.length; i++){
-        if(data[i].name == "Thành phố Hồ Chí Minh"){
-
-          // get all of districts
-          let districts = [] 
-          districts = data[i].districts
-
-          for(let j = 0; j < districts.length; j++){
-            console.log(districts[j])
-          }
-        } 
-      }
-
+      // console.log(data) 
+     
 
     } catch (error) {
       console.error('Error:', error);
@@ -121,22 +98,44 @@ export default function Map() {
 
   }
   fetchVietNameInfo();
-  const provider = new OpenStreetMapProvider();
+  // const provider = new OpenStreetMapProvider();
+
+  const searchAddressChangeHandle = (event) => {
+    setAddress(event.target.value);
+  }
+
+  const enterKeyPressHandle = (event) => {
+    if (event.key == "Enter"){
+      fetchAddressInfo(address)
+      
+      console.log("enter");
+    }
+  }
 
 
   return (
     <Stack className="map--wrap" direction={"row"} sx={{ position: "relative" }}>
-      <SideBar />
+      <SideBar className="map--wrap__sidebar"/>
       <MapContainer
         center={position}
         zoom={10}
-        className="map"
+        className="map--wrap__map"
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
+        
+        <Box className="map--wrap__map__search--field">
+          <Stack className="map--wrap__map__search--field__content" direction={"row"}> 
+            <input type="text" value={address} onChange={searchAddressChangeHandle} onKeyDown={enterKeyPressHandle}/>
+            <Box className="map--wrap__map__search--field__content__search--icon">
+             <SearchIcon sx={{cursor: "pointer", width:"100%", height: "100%"}}/>
+            </Box>
+          </Stack>
+        </Box>
 
+        
         {/* <SearchField
           provider={provider}
           showMarker={true}
@@ -150,59 +149,17 @@ export default function Map() {
           keepResult={true}
         /> */}
 
-        <Box
-          sx={{
-            position: "absolute",
-            backgroundColor: "White",
-            top: "2vh",
-            right: "1vw",
-            width: "60%",
-            height: "10%",
-            border: "1px solid lightgray",
-            borderRadius: "10px",
-            zIndex: "600"
-          }}
-        >
-          <Box component={"form"}>
-
-            {/* <Autocomplete
-              freeSolo
-              id="free-solo-2-demo"
-              disableClearable
-              options={top100Films.map((option) => option.title)}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Search input"
-                  InputProps={{
-                    ...params.InputProps,
-                    type: 'search',
-                  }}
-                />
-              )}
-            /> */}
-            
-          </Box>
-        </Box>
-
-        {(strAddress[1].length > 0) & (strAddress[0].length > 0) ? (
+        {(strAddress.latitude != "") & (strAddress.longitude != "") ? (
           <>
             <Marker
               position={[
-                strAddress[0][0].latitude1,
-                strAddress[0][1].longitude1,
-              ]}
-              icon={customIcon}
-            />
-            <Marker
-              position={[
-                strAddress[1][0].latitude2,
-                strAddress[1][1].longitude2,
+                strAddress.latitude,
+                strAddress.longitude,
               ]}
               icon={customIcon}
             />
           </>
-        ) : null}
+        ) : null} 
       </MapContainer>
     </Stack>
 
