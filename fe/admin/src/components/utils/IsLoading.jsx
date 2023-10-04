30d05acaf640bb3ff248c47325507423254c6aa9
PhuNguyen8902 <89370742+PhuNguyen8902@users.noreply.github.com>
2023-10-04 11:38:49 +0700

    admin part 2

--- fe/admin/src/components/utils/IsLoading.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, CircularProgress } from "@mui/material";
-
-export default function IsLoading() {
-  return (
-    <Box className="loading" sx={{ display: "flex" }}>
-      <CircularProgress />
-    </Box>
-  );
-}
--- fe/admin/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import AuthPage from "./AuthPage";
-import TripPage from "./TripPage";
-import RoutePage from "./RoutePage";
-import AuthenPage from "./AuthenPage";
-
-export { AuthPage, TripPage, RoutePage, AuthenPage };
